c66036384f6e9b2e38c53f5f4034652fa45233be
ganeshsai4408 <144612987+ganeshsai4408@users.noreply.github.com>
2025-08-04 17:12:59 +0530

    Update client.js

--- inngest/client.js
+++ inngest/client.js
@@ -1,5 +1,7 @@
 import {Inngest} from "inngest";
 export const inngest = new Inngest({
     id: 'Ticketting-system',
-    eventKey: process.env.INNGEST_EVENT_KEY
+    
+    eventKey: process.env.INNGEST_EVENT_KEY,
+    eventKey: process.env.INNGEST_EVENT_KEY,
 });
